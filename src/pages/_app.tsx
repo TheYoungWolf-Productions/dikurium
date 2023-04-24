d0721f080c5cf156e26e182457bcd6dbaa0b7ae3
theYoungWolf-Productions <imafraz@hotmail.com>
2023-04-25 01:28:39 +0500

    Add apollo and pass it via context to child tree

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// _app.tsx
+import { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { rtlCache } from "../rtl-cache";
+import { ApolloClient, createHttpLink, InMemoryCache, gql, ApolloProvider } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const httpLink = createHttpLink({
+  uri: "https://api.github.com/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+  return (
+    <div dir="rtl">
+      <MantineProvider theme={{ dir: "rtl" }} withGlobalStyles withNormalizeCSS emotionCache={rtlCache}>
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </MantineProvider>
+    </div>
+  );
 }
