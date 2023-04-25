ef4230a6c2f8269bb64cbbd094c1e5fe4a35e4df
theYoungWolf-Productions <imafraz@hotmail.com>
2023-04-25 05:17:44 +0500

    Fix lang direction

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,15 @@
 // _app.tsx
 import { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
-import { rtlCache } from "../rtl-cache";
-import { ApolloClient, createHttpLink, InMemoryCache, gql, ApolloProvider } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import { ApolloProvider } from "@apollo/client";
 
-const httpLink = createHttpLink({
-  uri: "https://api.github.com/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  return {
-    headers: {
-      ...headers,
-      authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import { client } from "@/lib/apollo-client";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
   return (
-    <div dir="rtl">
-      <MantineProvider theme={{ dir: "rtl" }} withGlobalStyles withNormalizeCSS emotionCache={rtlCache}>
+    <div dir="ltr">
+      <MantineProvider theme={{ dir: "ltr" }} withGlobalStyles withNormalizeCSS>
         <ApolloProvider client={client}>
           <Component {...pageProps} />
         </ApolloProvider>
